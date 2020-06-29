ddeec673340a2911fb2fb86131a69d07a1224058
Lê Quang Trung <39020860+quangtrungkhmt4@users.noreply.github.com>
2020-06-29 15:51:43 +0700

    add
    add

---
+++ PingPing-Server/src/test/java/com/ben/pingping/PingpingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ben.pingping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingpingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
